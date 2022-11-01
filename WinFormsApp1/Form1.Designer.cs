e4d0a5ba96201e881b9a449b105826e211ddd201
eddy <eddy19991118@gmail.com>
2022-11-01 16:45:06 +0800

    label to textbox

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -28,87 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.newGameButton = new System.Windows.Forms.Button();
-            this.guessNumberButton = new System.Windows.Forms.Button();
-            this.resultLabel = new System.Windows.Forms.Label();
-            this.inputTextBox = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // newGameButton
-            // 
-            this.newGameButton.Location = new System.Drawing.Point(271, 43);
-            this.newGameButton.Name = "newGameButton";
-            this.newGameButton.Size = new System.Drawing.Size(75, 23);
-            this.newGameButton.TabIndex = 0;
-            this.newGameButton.Text = "新遊戲";
-            this.newGameButton.UseVisualStyleBackColor = true;
-            this.newGameButton.Click += new System.EventHandler(this.newGameButton_Click);
-            // 
-            // guessNumberButton
-            // 
-            this.guessNumberButton.Location = new System.Drawing.Point(398, 97);
-            this.guessNumberButton.Name = "guessNumberButton";
-            this.guessNumberButton.Size = new System.Drawing.Size(75, 23);
-            this.guessNumberButton.TabIndex = 3;
-            this.guessNumberButton.Text = "猜猜看";
-            this.guessNumberButton.UseVisualStyleBackColor = true;
-            this.guessNumberButton.Click += new System.EventHandler(this.guessNumberButton_Click);
-            // 
-            // resultLabel
-            // 
-            this.resultLabel.AutoSize = true;
-            this.resultLabel.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.resultLabel.Location = new System.Drawing.Point(126, 152);
-            this.resultLabel.Name = "resultLabel";
-            this.resultLabel.Size = new System.Drawing.Size(48, 24);
-            this.resultLabel.TabIndex = 4;
-            this.resultLabel.Text = "結果";
-            // 
-            // inputTextBox
-            // 
-            this.inputTextBox.Location = new System.Drawing.Point(256, 97);
-            this.inputTextBox.Name = "inputTextBox";
-            this.inputTextBox.Size = new System.Drawing.Size(100, 23);
-            this.inputTextBox.TabIndex = 2;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(12, 97);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(238, 24);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "請輸入四個不相同的數字：";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(398, 47);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(238, 24);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "要先啟動遊戲才能猜猜看唷";
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(653, 679);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.inputTextBox);
-            this.Controls.Add(this.resultLabel);
-            this.Controls.Add(this.guessNumberButton);
-            this.Controls.Add(this.newGameButton);
-            this.Name = "Form1";
-            this.Text = "xAxB猜數字遊戲";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.newGameButton = new System.Windows.Forms.Button();
+			this.guessNumberButton = new System.Windows.Forms.Button();
+			this.inputTextBox = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.resultTextBox = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// newGameButton
+			// 
+			this.newGameButton.Location = new System.Drawing.Point(271, 43);
+			this.newGameButton.Name = "newGameButton";
+			this.newGameButton.Size = new System.Drawing.Size(75, 23);
+			this.newGameButton.TabIndex = 0;
+			this.newGameButton.Text = "新遊戲";
+			this.newGameButton.UseVisualStyleBackColor = true;
+			this.newGameButton.Click += new System.EventHandler(this.newGameButton_Click);
+			// 
+			// guessNumberButton
+			// 
+			this.guessNumberButton.Location = new System.Drawing.Point(398, 97);
+			this.guessNumberButton.Name = "guessNumberButton";
+			this.guessNumberButton.Size = new System.Drawing.Size(75, 23);
+			this.guessNumberButton.TabIndex = 3;
+			this.guessNumberButton.Text = "猜猜看";
+			this.guessNumberButton.UseVisualStyleBackColor = true;
+			this.guessNumberButton.Click += new System.EventHandler(this.guessNumberButton_Click);
+			// 
+			// inputTextBox
+			// 
+			this.inputTextBox.Location = new System.Drawing.Point(256, 97);
+			this.inputTextBox.Name = "inputTextBox";
+			this.inputTextBox.Size = new System.Drawing.Size(100, 23);
+			this.inputTextBox.TabIndex = 2;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label2.Location = new System.Drawing.Point(12, 97);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(238, 24);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "請輸入四個不相同的數字：";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.label1.ForeColor = System.Drawing.Color.Red;
+			this.label1.Location = new System.Drawing.Point(398, 47);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(238, 24);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "要先啟動遊戲才能猜猜看唷";
+			// 
+			// resultTextBox
+			// 
+			this.resultTextBox.Location = new System.Drawing.Point(126, 152);
+			this.resultTextBox.Multiline = true;
+			this.resultTextBox.Name = "resultTextBox";
+			this.resultTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.resultTextBox.Size = new System.Drawing.Size(347, 297);
+			this.resultTextBox.TabIndex = 6;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(642, 495);
+			this.Controls.Add(this.resultTextBox);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.inputTextBox);
+			this.Controls.Add(this.guessNumberButton);
+			this.Controls.Add(this.newGameButton);
+			this.Name = "Form1";
+			this.Text = "xAxB猜數字遊戲";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -116,9 +115,9 @@
 
         private Button newGameButton;
         private Button guessNumberButton;
-        private Label resultLabel;
         private TextBox inputTextBox;
         private Label label2;
         private Label label1;
+        private TextBox resultTextBox;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -3,215 +3,205 @@ using System.Numerics;
 
 namespace WinFormsApp1
 {
-    public partial class Form1 : Form
-    {
-        private GuessNumber game;
-        public Form1()
-        {
-            InitializeComponent();
-            game = new GuessNumber();
-            resultLabel.Text = String.Empty;
-            guessNumberButton.Enabled = false;
-        }
+	public partial class Form1 : Form
+	{
+		private GuessNumber game;
+		public Form1()
+		{
+			InitializeComponent();
+			game = new GuessNumber();
+			resultTextBox.Text = String.Empty;
 
-        private void newGameButton_Click(object sender, EventArgs e)
-        {
-            //啟動新一輪遊戲，抓取1~9四個不相同的數字
-            guessNumberButton.Enabled = true;
-            game.NewGame();
-            resultLabel.Text = game.Hint;
-        }
+			guessNumberButton.Enabled = false;
+		}
 
-        private void guessNumberButton_Click(object sender, EventArgs e)
-        {
-            int[] inputNumber = new int[4];
-            //1.抓取使用者輸入
-            try
-            {
-                inputNumber = GetNumber();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-                return;
-            }
+		private void newGameButton_Click(object sender, EventArgs e)
+		{
+			//啟動新一輪遊戲，抓取1~9四個不相同的數字
+			guessNumberButton.Enabled = true;
+			game.NewGame();
+			resultTextBox.Text = game.Hint;
 
-            //2.判斷幾A幾B
-            GuessResult result = game.Guess(inputNumber);
-            if (result.IsSuccess == true)
-            {
-                MessageBox.Show("您答對了");
-                guessNumberButton.Enabled = false;
-            }
-            else
-            {
-                MessageBox.Show("您答錯了");
-            }
+		}
 
-            //3.呈現資訊
-            string inputAnswer = string.Empty;
-            foreach (int i in inputNumber)
-            {
-                inputAnswer += i.ToString() + " ";
-            }
-            game.AddResultString($"您目前的答案{inputAnswer}", $"{result.Hint}");
-            resultLabel.Text = game.Hint;
-            //string inputAnswer = string.Empty;
-            //foreach (int i in inputNumber)
-            //{
-            //    inputAnswer += i.ToString() + " ";
-            //}
-            //game.AddResultString($"您目前的答案{inputAnswer}", $"0 A 0 B");
-            //resultLabel.Text = game.Hint;
+		private void guessNumberButton_Click(object sender, EventArgs e)
+		{
+			int[] inputNumber = new int[4];
+			//1.抓取使用者輸入
+			try
+			{
+				inputNumber = GetNumber();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+				return;
+			}
 
-        }
-        /// <summary>
-        /// 取得使用者輸入
-        /// </summary>
-        /// <returns>回傳四個不相同的數字</returns>
-        /// <exception cref="Exception"></exception>
-        private int[] GetNumber()
-        {
-            string input = inputTextBox.Text;
-            int[] arInputNumber = new int[4];
-            char[] originalArNumber = input.ToCharArray();
-            char[] arNumber = input.ToCharArray();//將字串轉成字元
-            Array.Sort(arNumber);//排序
+			//2.判斷幾A幾B
+			GuessResult result = game.Guess(inputNumber);
+			if (result.IsSuccess == true)
+			{
+				MessageBox.Show("您答對了");
+				guessNumberButton.Enabled = false;
+			}
+			else
+			{
+				MessageBox.Show("您答錯了");
+			}
 
-            //if (string.IsNullOrEmpty(input)) throw new Exception("請輸入數字");
-            bool isInt = int.TryParse(input, out int number);//判斷有沒有轉成功
-            if (input.Length != 4) throw new Exception("請輸入四個數字");//如果不為四個數字，拋出例外
-            if (!isInt) throw new Exception("請輸入數字");//如果轉失敗，拋出例外
-            //判斷使用者輸入的數字，有沒有一樣的數字，有就拋出例外
-            for (int i = 1; i < input.Length; i++)
-            {
-                if (arNumber[i] == arNumber[i - 1])
-                {
-                    throw new Exception("數字有重複，請重新輸入謝謝");
-                }
-            }
-            //把字串存入陣列裡面
-            for (int i = 0; i < originalArNumber.Length; i++)
-            {
-                arInputNumber[i] = Convert.ToInt32(originalArNumber[i]) - 48;
-            }
-            //foreach (int i in arNumber)
-            //{
-            //    arInputNumber[i] = Convert.ToInt32(arNumber[i]) + 48;
-            //}
+			//3.呈現資訊
+			string inputAnswer = string.Empty;
+			foreach (int i in inputNumber)
+			{
+				inputAnswer += i.ToString() + " ";
+			}
+			game.AddResultString($"您目前的答案{inputAnswer}", $"{result.Hint}");
+			resultTextBox.Text = game.Hint;
+			//string inputAnswer = string.Empty;
+			//foreach (int i in inputNumber)
+			//{
+			//    inputAnswer += i.ToString() + " ";
+			//}
 
-            //如果上面判斷完沒問題，就回傳目前使用者輸入的值(陣列)
-            return arInputNumber;
-        }
-    }
+		}
+		/// <summary>
+		/// 取得使用者輸入
+		/// </summary>
+		/// <returns>回傳四個不相同的數字</returns>
+		/// <exception cref="Exception"></exception>
+		private int[] GetNumber()
+		{
+			string input = inputTextBox.Text;
+			int[] arInputNumber = new int[4];
+			char[] originalArNumber = input.ToCharArray();
+			char[] arNumber = input.ToCharArray();//將字串轉成字元
+			Array.Sort(arNumber);//排序
 
-    public class GuessNumber
-    {
-        public int[] NumberAnswer = new int[4];//答案
-        public string result { get; set; }
-        public void NewGame()
-        {
-            result = string.Empty;
-            int seed = Guid.NewGuid().GetHashCode();//亂數種子
-            Random random = new Random(seed);
-            //取四個不相同的數字
-            for (int i = 0; i <= 3; i++)
-            {
-                int randomNumber = random.Next(1, 10);//1~10取亂數
-                this.NumberAnswer[i] = randomNumber;
-                for (int j = 0; j <= i - 1; j++)
-                {
-                    while (this.NumberAnswer[i] == this.NumberAnswer[j])
-                    {
-                        j = 0;
-                        this.NumberAnswer[i] = random.Next(1, 10);
-                    }
-                }
-            }
-            string correctAnswer = string.Empty;
-            foreach (int i in this.NumberAnswer)
-            {
-                correctAnswer += i.ToString() + " ";
-            }
-            AddResultString($"正確的答案是{correctAnswer}", "0 A 0 B");
-        }
+			//if (string.IsNullOrEmpty(input)) throw new Exception("請輸入數字");
+			bool isInt = int.TryParse(input, out int number);//判斷有沒有轉成功
+			if (input.Length != 4) throw new Exception("請輸入四個數字");//如果不為四個數字，拋出例外
+			if (!isInt) throw new Exception("請輸入數字");//如果轉失敗，拋出例外
+			//判斷使用者輸入的數字，有沒有一樣的數字，有就拋出例外
+			for (int i = 1; i < input.Length; i++)
+			{
+				if (arNumber[i] == arNumber[i - 1])
+				{
+					throw new Exception("數字有重複，請重新輸入謝謝");
+				}
+			}
+			//把字串存入陣列裡面
+			for (int i = 0; i < originalArNumber.Length; i++)
+			{
+				arInputNumber[i] = Convert.ToInt32(originalArNumber[i]) - 48;
+			}
 
-        public string Hint
-        {
-            get
-            {
-                return result;
-            }
-            set
-            {
-                result = value;
-            }
-        }
+			//如果上面判斷完沒問題，就回傳目前使用者輸入的值(陣列)
+			return arInputNumber;
+		}
+	}
 
-        public string AddResultString(string first, string second)
-        {
-            return result += $"{first}            目前是{second}\r\n";
-        }
+	public class GuessNumber
+	{
+		public int[] NumberAnswer = new int[4];//答案
+		public string result { get; set; }
+		public void NewGame()
+		{
+			result = string.Empty;
+			int seed = Guid.NewGuid().GetHashCode();//亂數種子
+			Random random = new Random(seed);
+			//取四個不相同的數字
+			for (int i = 0; i <= 3; i++)
+			{
+				int randomNumber = random.Next(1, 10);//1~10取亂數
+				this.NumberAnswer[i] = randomNumber;
+				for (int j = 0; j <= i - 1; j++)
+				{
+					while (this.NumberAnswer[i] == this.NumberAnswer[j])
+					{
+						j = 0;
+						this.NumberAnswer[i] = random.Next(1, 10);
+					}
+				}
+			}
+			string correctAnswer = string.Empty;
+			foreach (int i in this.NumberAnswer)
+			{
+				correctAnswer += i.ToString() + " ";
+			}
+			AddResultString($"正確的答案是{correctAnswer}", "0 A 0 B");
+		}
 
-        public GuessResult Guess(int[] number)
-        {
-            //判斷幾A幾B
-            int bingoCountA = 0;
-            int bingoCountB = 0;
-            for (int i = 0; i < number.Length; i++)
-            {
-                if (NumberAnswer[i] == number[i])//如果一樣就是A++
-                {
-                    bingoCountA++;
-                }
-                else//不一樣就比較全部，如果有一樣B++
-                {
-                    for (int j = 0; j < number.Length; j++)
-                    {
-                        if (NumberAnswer[i] == number[j])
-                        {
-                            bingoCountB++;
-                        }
-                    }
-                }
-            }
-            //foreach (var item in number)
-            //{
-            //    if (NumberAnswer[item] == number[item])
-            //    {
-            //        bingoCount++;
-            //    }
-            //}
-            if (bingoCountA == 4)//答對了
-            {
-                return GuessResult.Success($"{bingoCountA} A {bingoCountB} B\r\n恭喜答對了");
-            }
-            else//答錯了
-            {
-                return GuessResult.Failed($"{bingoCountA} A {bingoCountB} B");
-            }
+		public string Hint
+		{
+			get
+			{
+				return result;
+			}
+			set
+			{
+				result = value;
+			}
+		}
 
-        }
-    }
-    public class GuessResult
-    {
-        public bool IsSuccess { get; set; }
-        public string Hint { get; set; }
-        public static GuessResult Success(string sucessMessage)
-        {
-            return new GuessResult { IsSuccess = true, Hint = sucessMessage };
-        }
-        public static GuessResult Failed(string errMessage)
-        {
-            return new GuessResult { IsSuccess = false, Hint = errMessage };
-        }
-        public bool IsFailed
-        {
-            get
-            {
-                return !IsSuccess;
-            }
-        }
-    }
+		public string AddResultString(string first, string second)
+		{
+			return result += $"{first}            目前是{second}\r\n";
+		}
+
+		public GuessResult Guess(int[] number)
+		{
+			//判斷幾A幾B
+			int bingoCountA = 0;
+			int bingoCountB = 0;
+			for (int i = 0; i < number.Length; i++)
+			{
+				if (NumberAnswer[i] == number[i])//如果一樣就是A++
+				{
+					bingoCountA++;
+				}
+				else//不一樣就比較全部，如果有一樣B++
+				{
+					for (int j = 0; j < number.Length; j++)
+					{
+						if (NumberAnswer[i] == number[j])
+						{
+							bingoCountB++;
+						}
+					}
+				}
+			}
+
+			if (bingoCountA == 4)//答對了
+			{
+				return GuessResult.Success($"{bingoCountA} A {bingoCountB} B\r\n恭喜答對了");
+			}
+			else//答錯了
+			{
+				return GuessResult.Failed($"{bingoCountA} A {bingoCountB} B");
+			}
+
+		}
+	}
+	public class GuessResult
+	{
+		public bool IsSuccess { get; set; }
+		public string Hint { get; set; }
+		public static GuessResult Success(string sucessMessage)
+		{
+			return new GuessResult { IsSuccess = true, Hint = sucessMessage };
+		}
+		public static GuessResult Failed(string errMessage)
+		{
+			return new GuessResult { IsSuccess = false, Hint = errMessage };
+		}
+		public bool IsFailed
+		{
+			get
+			{
+				return !IsSuccess;
+			}
+		}
+	}
 
 }
